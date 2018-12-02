8c9ff3194e8526909b5b6a02feee704f8f4c9c67
Alejandro Proano <alejandro.proano@verizondigitalmedia.com>
2018-12-01 19:08:01 -0800

    Add 4 categories with 3 items each

--- lotsofitems.py
+++ lotsofitems.py
@@ -31,10 +31,21 @@ category1 = Category(name="Summer Activities")
 session.add(category1)
 session.commit()
 
-item1 = Item(user_id=1, name="Surf Camp", 
+item1 = Item(user_id=1, name="Surf", 
              description="It's the most fun and cost efficient way to spend a holiday surfing and meeting people with the same desires to learn a sport that will hook you from the get go.", category=category1)
 
 session.add(item1)
+session.commit()
+
+item2 = Item(user_id=1, name="Basketball",
+             description="Players will learn the fundamentals while participating in skills and drills and will have the opportunity to scrimmage during a tournament at the end of the week.", category=category1)
+
+session.add(item2)
+session.commit()
+
+item3 = Item(user_id=1, name="Flag Football",
+             description="Develop fundamental skills and strategies such as passing, throwing, catching, blocking, and defending/flag-pulling.", category=category1)
+session.add(item3)
 session.commit()
 
 
@@ -43,14 +54,64 @@ category2 = Category(name="Winter Activities")
 session.add(category2)
 session.commit()
 
+
+item4 = Item(user_id=1, name="Ice Fishing",
+             description="Topics include ice safety, how to start and run a power auger, learn to set up a portable shack, set tips ups, use a jig rod, fish with live and artificial bait, how to use electronics, view life under the ice with the underwater camera and more", category=category2)
+session.add(item4)
+session.commit()
+
+
+item5 = Item(user_id=1, name="Figure Skating",
+             description="Not only does figure skating help young children develop coordination, strength, and agility, it helps to develop their minds. Even at the lowest basic skills levels skaters learn step sequences and can even learn a complete program to be used at local competitions.", category=category2)
+session.add(item5)
+session.commit()
+
+item6 = Item(user_id=1, name="Hockey",
+             description="Skaters will learn the basics of skating, puck handling, passing, shooting, and many of the other skills involved in the game of ice hockey.", category=category2)
+session.add(item6)
+session.commit()
+
+
 category3 = Category(name="Fall Activities")
 
 session.add(category3)
+session.commit()
+
+item7 = Item(user_id=1, name="Gymnastics",
+             description="Our gym program includes floor and tumbling, balance beam, vaulting horse, uneven bars, rhythmical gymnastics.", category=category3)
+session.add(item7)
+session.commit()
+
+item8 = Item(user_id=1, name="Fencing",
+             description="Activities include Group lessons, Competition, Footwork, Games, and Technique & Tactics", category=category3)
+session.add(item8)
+session.commit()
+
+item9 = Item(user_id=1, name="Drama",
+             description=" Activities include play directing, set design, improv acting, singing, and music.", category=category3)
+
+session.add(item9)
 session.commit()
 
 category4 = Category(name="Spring Activities")
 
 session.add(category4)
 session.commit()
+
+item10 = Item(user_id=1, name="Hicking",
+             description="Hiking is a healthy low-impact way to exercise and enjoy the outdoors. Whether you are looking to challenge yourself, explore cultural landmarks, or just 'get away from it all'", category=category4)
+session.add(item10)
+session.commit()
+
+item11 = Item(user_id=1, name="Backpacking",
+             description="Backpacking is an adventure that blends hiking with backcountry camping. It lets you broaden your horizons beyond the car campground to enjoy a richer, more immersive outdoor experience", category=category4)
+session.add(item11)
+session.commit()
+
+item12 = Item(user_id=1, name="Camping",
+              description="Camping is like staying in a primitive cabin, minus the cabin itself. So, in addition to your tent, pack as though you're going to stay someplace where there's little or no furniture, no electricity, no stove or refrigerator, and the cupboards are bare.", category=category4)
+session.add(item12)
+session.commit()
+
 
 print "added items!"
